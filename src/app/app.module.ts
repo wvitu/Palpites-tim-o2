2179c7f9fa240f061240cb61beca2dcc1995fad0
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-05-08 22:19:20 -0300

    Resultado final
    Cria componente Resultado Final com inputs para placar real e botão de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MatchInfoComponent } from './match-info/match-info.component';
 import { PalpitesComponent } from './palpites/palpites.component';
+import { ResultadoFinalComponent } from './resultado-final/resultado-final.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MatchInfoComponent,
-    PalpitesComponent
+    PalpitesComponent,
+    ResultadoFinalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/resultado-final/resultado-final.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado-final',
+  templateUrl: './resultado-final.component.html',
+  styleUrls: ['./resultado-final.component.css']
+})
+export class ResultadoFinalComponent {
+
+}
